5e239aa35a53c92da96a8f2c86009541bfb4cf84
JensDiemer <git@jensdiemer.de>
2022-11-10 20:44:34 +0100

    Add a simple dump serial output script

---
+++ pysmartmeter/dump.py
@@ -0,0 +1,70 @@
+import sys
+
+import serial
+from serial.tools.list_ports import comports
+
+
+def get_serial(
+    baudrate=9600,
+    parity=serial.PARITY_EVEN,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.SEVENBITS,
+    timeout=1,
+    terminator=b'\r\n',
+    verbose=True,
+):
+    """
+    Get the first "working" serial instance back.
+    """
+    for port, desc, hwid in comports(include_links=False):
+        if verbose:
+            print(f'try: {port} {desc} {hwid}')
+        try:
+            ser = serial.Serial(
+                port,
+                baudrate=baudrate,
+                parity=parity,
+                stopbits=stopbits,
+                bytesize=bytesize,
+                timeout=timeout,
+            )
+        except Exception as err:
+            if verbose:
+                print(f'ERROR: {err}')
+            continue
+
+        if verbose:
+            print(f'Read from {ser}...', end=' ', flush=True)
+        try:
+            data = ser.readline()
+        except Exception as err:
+            if verbose:
+                print(f'ERROR: {err}')
+            continue
+        else:
+            if verbose:
+                print(data)
+            if data.endswith(terminator):
+                return ser
+
+
+def dump():
+    """
+    Dump the output of the first working serial port.
+    """
+    ser = get_serial()
+    if not ser:
+        print('Serial not found')
+        sys.exit(1)
+
+    while True:
+        data = ser.readline()
+        data = data.decode('ASCII')
+        print(data.rstrip())
+
+
+if __name__ == '__main__':
+    try:
+        dump()
+    except KeyboardInterrupt:
+        print('\nbye, bye\n')
